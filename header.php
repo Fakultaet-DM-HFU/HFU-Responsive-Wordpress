9296f0a38611ca588a95332e18486d57dc9dfaf5
Nio-av <public@nio-av.de>
2015-08-03 14:11:59 +0200

    Improved PlugIn-Support

--- header.php
+++ header.php
@@ -6,6 +6,12 @@
     <title>
         <?php wp_title(); if ( is_front_page()==false){ echo " - "; } bloginfo( 'name'); ?>
     </title>
+    
+    <!-- For Ping -->
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+    <?php wp_head(); ?>
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="<?php echo(get_template_directory_uri()); ?>/style.css">
